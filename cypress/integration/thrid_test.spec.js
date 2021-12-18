ff0ed798705225ab29b83da554c78b4a569f9d15
wissanu <thungrut@gmail.com>
2021-12-18 16:49:51 +0700

    fix testing cypress new function v.0.2.3

--- cypress/integration/thrid_test.spec.js
+++ cypress/integration/thrid_test.spec.js
@@ -2,6 +2,14 @@
 
 context('thrid test', () => {
 
+    const withTextCompareOptions = {
+		failureThreshold: 0.05,
+		failureThresholdType: 'percent' // not actually percent - fraction
+	};
+	const withMuchTextCompareOptions = {
+		failureThreshold: 0.08,
+		failureThresholdType: 'percent' // not actually percent - fraction
+	};
 	const toolboxCompareOptions = {
 		failureThreshold: 40,
 		failureThresholdType: 'pixel'
@@ -13,15 +21,17 @@ context('thrid test', () => {
 		cy.window().should('have.property', 'get_tool_by_id'); // wait for app to be loaded
 		cy.matchImageSnapshot(withTextCompareOptions);
 	});
-    
+
     it('brush selected', () => {
 		cy.get('.tool[title="Brush"]').click();
 		cy.get('.tools-component').matchImageSnapshot(toolboxCompareOptions);
 	});
+
 	it('select selected', () => {
 		cy.get('.tool[title="Select"]').click();
 		cy.get('.tools-component').matchImageSnapshot(toolboxCompareOptions);
 	});
+    
 	it('magnifier selected', () => {
 		cy.get('.tool[title="Magnifier"]').click();
 		cy.get('.tools-component').matchImageSnapshot(toolboxCompareOptions);
